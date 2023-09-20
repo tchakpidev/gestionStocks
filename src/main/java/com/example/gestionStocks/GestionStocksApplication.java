3574c64fb6010a8efb3c6f51cd536d9250d7d403
tchakpidev <tchakpipatokibalomelaine@gmail.com>
2023-09-20 10:33:56 +0000

    first commit

---
+++ src/main/java/com/example/gestionStocks/GestionStocksApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionStocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestionStocks/GestionStocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionStocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionStocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
